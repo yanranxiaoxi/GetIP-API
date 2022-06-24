e68abc0b70958432f8d3243ef926c55db5c645e9
久治明千树汐 <admin@soraharu.com>
2022-06-24 18:06:21 +0800

    First commit

---
+++ api/index.php
@@ -0,0 +1,22 @@
+<?php
+function getIP()
+{ 
+    foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key)
+    {
+        if (array_key_exists($key, $_SERVER) === true)
+        {
+            foreach (array_map('trim', explode(',', $_SERVER[$key])) as $ip)
+            {
+                if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false)
+                {
+                    return $ip;
+                }
+            }
+        }
+    }
+}
+
+$users_ip = getIP();
+
+echo $users_ip; // Output IP address [Ex: 123.45.67.89]
+?>
